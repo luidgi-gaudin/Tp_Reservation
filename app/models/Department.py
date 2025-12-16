d5e9f5ce09da11196aa164fe15e53e9fd9cd233b
luidgi <146814537+luidgi-gaudin@users.noreply.github.com>
2025-12-16 14:33:33 +0100

    Add `site_id` field to `Ressource` model with foreign key constraint.

--- app/models/Department.py
+++ app/models/Department.py
@@ -1,17 +1,21 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field, field_validator
+from sqlmodel import SQLModel, Field
+from sqlalchemy.orm import validates
 from app.models.Site import Site
 from app.models.User import User
 
 
-class Department(BaseModel):
+class Department(SQLModel, table=True):
+    __tablename__ = "departments"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
     nom: str = Field(min_length=3)
-    site: Site
-    manager: User
+    site: Site = Field(foreign_key="sites.id")
+    manager: User = Field(foreign_key="users.id")
     budgetAnnuel: Optional[float] = Field(ge=0)
 
-    @field_validator("manager")
+    @validates("manager")
     def user_must_be_manager(cls, manager: User) -> User:
         if manager.role not in ["manager", "admin"]:
             raise ValueError(f"L'utilisateur sélectionné doit etre manager ou admin !  (role actuel: {manager.role})")
