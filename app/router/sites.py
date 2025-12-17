d115f40516a52c8480abecb0d40b472624346832
luidgi <146814537+luidgi-gaudin@users.noreply.github.com>
2025-12-17 09:27:21 +0100

    Add `GET` and `PUT` endpoints for `Site`, refactor time conversion logic into `traduction_str_heure`, and update HTTP tests.

--- app/router/sites.py
+++ app/router/sites.py
@@ -7,18 +7,19 @@ from datetime import time as time_type
 
 site_router = APIRouter(prefix="/sites", tags=["sites"])
 
+def traduction_str_heure(heure: str) -> time_type:
+    h, m, s = map(int, heure.split(':'))
+    return time_type(h, m, s)
 
 
 @site_router.post("/")
 def create_site(site: Site, session: SessionDep) -> Site | dict[str, str]:
     try:
         if isinstance(site.horaires_ouverture, str):
-            h, m, s = map(int, site.horaires_ouverture.split(':'))
-            site.horaires_ouverture = time_type(h, m, s)
+            site.horaires_ouverture = traduction_str_heure(site.horaires_ouverture)
 
         if isinstance(site.horaires_fermeture, str):
-            h, m, s = map(int, site.horaires_fermeture.split(':'))
-            site.horaires_fermeture = time_type(h, m, s)
+            site.horaires_fermeture = traduction_str_heure(site.horaires_fermeture)
 
         session.add(site)
         session.commit()
@@ -43,3 +44,29 @@ def delete_site(site_id: int, session: SessionDep):
     session.delete(site)
     session.commit()
     return {"ok": True}
+
+@site_router.get("/{site_id}")
+def get_site(site_id: int, session: SessionDep):
+    site = session.get(Site, site_id)
+    if not site:
+        raise HTTPException(status_code=404, detail="Site Introuvable")
+    return site
+
+@site_router.put("/{site_id}")
+def update_site(site_id: int, site: Site, session: SessionDep):
+
+    if isinstance(site.horaires_ouverture, str):
+        site.horaires_ouverture = traduction_str_heure(site.horaires_ouverture)
+
+    if isinstance(site.horaires_fermeture, str):
+        site.horaires_fermeture = traduction_str_heure(site.horaires_fermeture)
+
+    site_db = session.get(Site, site_id)
+    if not site_db:
+        raise HTTPException(status_code=404, detail="Site Introuvable")
+    site_data = site.model_dump(exclude_unset=True)
+    site_db.sqlmodel_update(site_data)
+    session.add(site_db)
+    session.commit()
+    session.refresh(site_db)
+    return site_db
